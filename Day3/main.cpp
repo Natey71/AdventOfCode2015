478be12bcc88034efea11408826569304e0a5ce8
Natey71 <natedog7120@gmail.com>
2025-12-01 20:17:07 -0500

    task2 of day3, track santa and robo santa

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -11,32 +11,56 @@ int main() {
         return 1;  
     }
     std::string line;
-    // Starting position
-    int x = 0;
-    int y = 0;
+    // Starting position Santa
+    int xS = 0;
+    int yS = 0;
+    // Starting position Robo 
+    int xR = 0;
+    int yR = 0;
     // To track visited houses
     std::set<std::pair<int, int>> visited;
     // Add starting position to visited
-    visited.insert({x, y});
+    // Both start at same position so add once
+    visited.insert({xS, yS});
     while(std::getline(file, line)) {
-        for(char &c : line) {
+        for(size_t i = 0; i < line.size(); ++i) {
+            // Get index of line to decide who moves
+            char c = line[i];
             // Move based on character
             // ^ = up, v = down, > = right, < = left
             // ^ add to y, v subtract from y
             // > add to x, < subtract from x
-            if(c == '^') {
-                y += 1;
-            } else if(c == 'v') {
-                y -= 1;
-            } else if(c == '>') {
-                x += 1;
-            } else if(c == '<') {
-                x -= 1;
+            // Santa's turn
+            if(i % 2 == 0) { 
+                if(c == '^') {
+                    yS += 1;
+                } else if(c == 'v') {
+                    yS -= 1;
+                } else if(c == '>') {
+                    xS += 1;
+                } else if(c == '<') {
+                    xS -= 1;
+                }
+                // Add Santa position to visited
+                visited.insert({xS, yS});
+            // Robo turns
+            } else { 
+                if(c == '^') {
+                    yR += 1;
+                } else if(c == 'v') {
+                    yR -= 1;
+                } else if(c == '>') {
+                    xR += 1;
+                } else if(c == '<') {
+                    xR -= 1;
+                }
+                // Add Robo's position to visited
+                visited.insert({xR, yR});
             }
-            // Record the new position
-            visited.insert({x, y});
         }
     }
     
+    // Total visited houses
     std::cout << "Visited Houses: " << visited.size() << std::endl;
+    return 0;
 }
