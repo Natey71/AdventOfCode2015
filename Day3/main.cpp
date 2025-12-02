7809ad237d8292ce718526b8e262c5eb19a41bce
Natey71 <natedog7120@gmail.com>
2025-12-01 19:14:38 -0500

    i was trying to use vector had to look up for some help what to use

--- Day3/main.cpp
+++ Day3/main.cpp
@@ -11,12 +11,19 @@ int main() {
         return 1;  
     }
     std::string line;
+    // Starting position
     int x = 0;
     int y = 0;
+    // To track visited houses
     std::set<std::pair<int, int>> visited;
+    // Add starting position to visited
     visited.insert({x, y});
     while(std::getline(file, line)) {
         for(char &c : line) {
+            // Move based on character
+            // ^ = up, v = down, > = right, < = left
+            // ^ add to y, v subtract from y
+            // > add to x, < subtract from x
             if(c == '^') {
                 y += 1;
             } else if(c == 'v') {
@@ -26,8 +33,10 @@ int main() {
             } else if(c == '<') {
                 x -= 1;
             }
+            // Record the new position
             visited.insert({x, y});
         }
     }
+    
     std::cout << "Visited Houses: " << visited.size() << std::endl;
 }
